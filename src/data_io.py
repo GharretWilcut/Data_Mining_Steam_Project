c937746e1e7fae761f86e11f9040db8c8dbed402
Malizma333 <malizma333@gmail.com>
2026-04-11 18:37:59 -0500

    prep data clean steps

--- src/data_io.py
+++ src/data_io.py
@@ -3,17 +3,15 @@ import sqlite3
 
 import pandas as pd
 
-FILE = "data/steam_games_dataset.db"
 
-
-def read_data():
-    conn = sqlite3.connect(FILE)
+def read_data(location: str):
+    conn = sqlite3.connect(location)
     df = pd.read_sql(sql="SELECT * FROM games", con=conn)
     conn.close()
     return df
 
 
-def write_data(df: pd.DataFrame):
-    conn = sqlite3.connect(FILE)
+def write_data(df: pd.DataFrame, location: str):
+    conn = sqlite3.connect(location)
     df.to_sql("games", conn, if_exists="replace", index=False)
     conn.close()
