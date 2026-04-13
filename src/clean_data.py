a614b64aff47f3c37b0e3ab7e6355fddca198453
Malizma333 <malizma333@gmail.com>
2026-04-13 01:36:05 -0500

    added preprocessing steps

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,79 +1,133 @@
 # Run data preprocessing step
 
+import json
+from datetime import datetime
+
 import pandas as pd
+from sklearn import model_selection
+from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer, StandardScaler
 
 from data_io import read_data, write_data
 
-"""
-Game Format
+standard_scaler = StandardScaler()
+label_binarizer = MultiLabelBinarizer()
+label_encoder = LabelEncoder()
 
-{
-    "appID": int
-    "name": string
-    "release_date": string
-    "estimated_owners": int
-    "peak_ccu": int
-    "required_age": int
-    "price": float
-    "dlc_count": int
-    "detailed_description": string
-    "short_description": string
-    "supported_languages": string[]
-    "full_audio_languages": string[]
-    "reviews": string
-    "header_image": string
-    "website": string
-    "support_url": string
-    "support_email": string
-    "windows": bool
-    "mac": bool
-    "linux": bool
-    "metacritic_score": int
-    "metacritic_url": string
-    "user_score": int
-    "positive": int
-    "negative": int
-    "score_rank": string
-    "achievements": int
-    "recommendations": int
-    "notes": string
-    "average_playtime_forever": int
-    "average_playtime_2weeks": int
-    "median_playtime_forever": int
-    "median_playtime_2weeks": int
-    "developers": string[]
-    "publishers": string[]
-    "categories": string[]
-    "genres": string[]
-    "tags": string[]
-    "screenshots": string[]
-    "movies": string[]
-    "packages": {
-        "title": string
-        "description": string
-        "subs": {
-            "text": string
-            "description": string
-            "price": float
-        }[]
-    }[]
-}
-"""
+
+def preprocess(df: pd.DataFrame, fit: bool):
+    clean_df = pd.DataFrame()
+
+    # Direct features
+    clean_df["price"] = df["price"]
+    clean_df["dlc_count"] = df["dlc_count"]
+
+    # Date when dataset was created, used for age calculation
+    reference_date = datetime(2026, 2, 2)
+    # Extract date features
+    dates = df["release_date"].map(lambda x: datetime.strptime(x, "%b %d, %Y"))
+    clean_df["day_of_year"] = dates.map(lambda x: x.timetuple().tm_yday / 366)
+    clean_df["age_in_days"] = dates.map(lambda x: (reference_date - x).days)
+
+    # Label-encode estimated_owners and move to y
+    if fit:
+        y = pd.Series(label_encoder.fit_transform(df["estimated_owners"]))
+    else:
+        y = pd.Series(label_encoder.transform(df["estimated_owners"]))
+
+    # Boolean casted features
+    clean_df["has_achievements"] = df["achievements"].map(lambda x: 0 if x == 0 else 1)
+    clean_df["has_website"] = df["website"].map(lambda x: 0 if x == "" else 1)
+    clean_df["has_support_url"] = df["support_url"].map(lambda x: 0 if x == "" else 1)
+    clean_df["has_support_email"] = df["support_email"].map(
+        lambda x: 0 if x == "" else 1
+    )
+
+    # Platform int casts
+    clean_df["windows"] = df["windows"].map(int)
+    clean_df["mac"] = df["mac"].map(int)
+    clean_df["linux"] = df["linux"].map(int)
+
+    # One-hot encoding of list columns
+    list_columns = [
+        "genres",
+        "categories",
+        "supported_languages",
+        "full_audio_languages",
+    ]
+    for column in list_columns:
+        column_contents = df[column].map(lambda x: x.split(","))
+        if fit:
+            encoded = label_binarizer.fit_transform(column_contents)
+        else:
+            encoded = label_binarizer.transform(column_contents)
+        labels = pd.DataFrame(
+            encoded,
+            columns=label_binarizer.classes_,
+            index=df.index,
+        )
+        labels = labels.drop(columns=[""]).add_prefix(f"{column} ")
+        clean_df = clean_df.join(labels)
+
+    # Label encoding of required age
+    # 0-10 -> E (0)
+    # 11-15 -> T (1)
+    # 16+ -> M (2)
+    clean_df["required_age"] = df["required_age"].map(
+        lambda x: (0 if x <= 10 else 1 if x <= 15 else 2) / 2
+    )
+
+    # Count number of packages
+    clean_df["package_count"] = df["packages"].map(lambda x: len(json.loads(x)))
+
+    standard_columns = ["price", "dlc_count", "age_in_days", "package_count"]
+    if fit:
+        clean_df[standard_columns] = standard_scaler.fit_transform(
+            clean_df[standard_columns]
+        )
+    else:
+        clean_df[standard_columns] = standard_scaler.transform(
+            clean_df[standard_columns]
+        )
+
+    # TODO text embeddings/keyword extraction
+    text_columns = ["name", "short_description", "detailed_description", "notes"]
+
+    # TODO fetch from links + autoencoder
+    image_columns = ["header_image", "screenshots"]
+
+    # TODO Feature selection (e.g., variance threshold, model-based selection, PCA)
+
+    return clean_df, y
+
 
 if __name__ == "__main__":
-    df = read_data("steam_games_dataset.db")
+    df_full = read_data("steam_games_dataset.db")
+
+    print("Data loaded")
 
     # Hardcoded name imputation
-    df.at[44432, "name"] = "The Spookening"
+    df_full.at[44432, "name"] = "The Spookening"
 
-    # TODO
-    # Train / Test Split
-    # Missing Value Handling
-    # Outlier Handling
-    # Feature Construction
-    # Categorical Encoding
-    # Feature Scaling
-    # Feature Selection
-    clean_df = pd.DataFrame(df)
+    df_train: pd.DataFrame
+    df_test: pd.DataFrame
+    df_train, df_test = model_selection.train_test_split(
+        df_full, test_size=0.25, random_state=42
+    )
 
-    write_data(clean_df, "steam_games_dataset_clean.db")
+    df_train_processed, y_train = preprocess(df_train, True)
+
+    print("Training preprocessed")
+
+    df_test_processed, y_test = preprocess(df_test, False)
+
+    print("Testing preprocessed")
+
+    df_train_processed["owners_label"] = y_train
+    df_test_processed["owners_label"] = y_test
+
+    print(df_train_processed)
+
+    write_data(df_train_processed, "steam_games_dataset_clean_training.db")
+    write_data(df_test_processed, "steam_games_dataset_clean_testing.db")
+
+    print("Data written")
